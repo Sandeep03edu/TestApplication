c566644bde145620684304bad897f6e0dcd15bd0
Sandeep Mishra <smishra@mathworks.com>
2024-10-22 15:09:53 +0530

    Added insecure shared preference handling

--- app/src/main/java/com/example/testapplication/MainActivity.kt
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.testapplication
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -12,5 +14,14 @@ class MainActivity : AppCompatActivity() {
 
         val mobSfAPIKey = "a51aa880c30337977d0107ba437c14cd41068be4f46f5c254acf0d7aa476ef23";
         println("mobSfAPI_Key: $mobSfAPIKey");
+
+
+        val sharedPreferences: SharedPreferences = applicationContext.getSharedPreferences("AppPreferences", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString("API_KEY", mobSfAPIKey)
+        editor.apply()
+
+        var key = sharedPreferences.getString("API_KEY", null)
+        key += "11";
     }
 }
